db28f0f2a0be61a2e6cf81d645ba748b03b09707
Blizire <stiles.trenton@gmail.com>
2022-04-17 02:47:21 -0500

    refactored frame diff function and commented code

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import win32gui
 import win32ui
 import win32con
-import itertools
 
 from PIL import Image
-from time import sleep
 
 # credit for this function:
 # https://stackoverflow.com/questions/1080719/screenshot-an-application-regardless-of-whats-in-front-of-it
@@ -39,26 +37,42 @@ def get_pixel_map(filename):
     with Image.open(filename) as im:
         return im.getdata()
 
-def scanner(hwnd, resolution):
-    width, height = resolution
-    max_pixel_count = width * height
-
-    background_screenshot(hwnd, resolution, filename="screenshot.bmp")
-    idle_pixel_map = get_pixel_map("screenshot.bmp")
-    
-    background_screenshot(hwnd, resolution, filename="active.bmp")
-    active_pixel_map = get_pixel_map("active.bmp")
-
+# gets the difference between two images and returns it as a percentage
+def get_image_difference(pixel_array_one, pixel_array_two, max_pixel_count):
     diff_score = 1
-    for (idle_pixel, active_pixel) in zip(idle_pixel_map, active_pixel_map):
+    # iterates through both arrays and compares if the pixels are different
+    for (idle_pixel, active_pixel) in zip(pixel_array_one, pixel_array_two):
         if idle_pixel == active_pixel:
             pass
         else:
             diff_score += 1
-    
+    # converts final total difference of pixels to a percentage
     diff_score_percentage = (diff_score / max_pixel_count) * 100
-    diff_score_percentage = str(round(diff_score_percentage, 2))
-    print(f"screen difference : {diff_score_percentage}%")
+    return round(diff_score_percentage, 2)
+    
+
+def scanner(hwnd, resolution):
+    # resolution should be the games resolution size
+    width, height = resolution
+    max_pixel_count = width * height
+
+    # this will capture the initial screen that should be of the idle lobby
+    background_screenshot(hwnd, resolution, filename="screenshot.bmp")
+    idle_pixel_map = get_pixel_map("screenshot.bmp")
+    
+    # todo: write loop that scans background window continously
+
+    # recapturing to see screen difference from here on out
+    background_screenshot(hwnd, resolution, filename="active.bmp")
+    active_pixel_map = get_pixel_map("active.bmp")
+
+    # compares the the screen compared to the idle lobby capture
+    # for different games the difference will have a negligible
+    # difference because of animated backgrounds.
+    diff_percentage = get_image_difference(idle_pixel_map, active_pixel_map, max_pixel_count)
+
+    print(f"screen difference : {diff_percentage}%")
+
     return
 
 
